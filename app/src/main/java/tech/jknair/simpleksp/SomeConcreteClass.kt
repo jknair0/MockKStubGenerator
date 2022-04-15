29e06e546e13b6aed02baefe8a72f496df714d3f
ByTerminator <jkdeveloper9@gmail.com>
2022-04-15 16:04:36 +0530

    added mockk stub generating support (#1)

---
+++ app/src/main/java/tech/jknair/simpleksp/SomeConcreteClass.kt
@@ -0,0 +1,13 @@
+package tech.jknair.simpleksp
+
+class SomeConcreteClass {
+
+    fun sum(a: Int, b: Int): Int {
+        return a + b
+    }
+
+    fun sum10(a: Int, b: Int = 10): Int {
+        return a + b
+    }
+
+}
--- stub/src/main/java/tech/jknair/stub/SampleClass.kt
+++ stub/src/main/java/tech/jknair/stub/SampleClass.kt
@@ -1,12 +1,14 @@
 package tech.jknair.stub
 
 import tech.jknair.processor.annotations.StubFactory
+import tech.jknair.simpleksp.SomeConcreteClass
 import tech.jknair.simpleksp.SomeOtherRepository
 import tech.jknair.simpleksp.SomeRepository
 
 @StubFactory(
     SomeRepository::class,
-    SomeOtherRepository::class
+    SomeOtherRepository::class,
+    SomeConcreteClass::class
 )
 interface SampleClass {
 
